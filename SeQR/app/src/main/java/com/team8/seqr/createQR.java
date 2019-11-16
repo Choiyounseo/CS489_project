c2886225a320e4a61728d33f21ede22bd8089377
Choiyounseo <dbstj72@kaist.ac.kr>
2019-11-16 20:19:45 +0900

    Add create, scan QR functions

--- SeQR/app/src/main/java/com/team8/seqr/createQR.java
+++ SeQR/app/src/main/java/com/team8/seqr/createQR.java
@@ -9,9 +9,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ImageView;
+import android.graphics.Bitmap;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class createQR extends Fragment {
+    private ImageView qrcodeIv;
+    private String text;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -19,5 +27,24 @@ public class createQR extends Fragment {
         View view =  inflater.inflate(R.layout.fragment_create_qr, container, false);
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        qrcodeIv = getView().findViewById(R.id.qrcode);
+
+        // TODO : need to change this part to use scanned information from other user..
+        // temporary creating fixed text information QRcode
+        text = "hello, world??";
+
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,200,200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            qrcodeIv.setImageBitmap(bitmap);
+        } catch (Exception e) {}
     }
 }
--- SeQR/app/src/main/java/com/team8/seqr/scanQR.java
+++ SeQR/app/src/main/java/com/team8/seqr/scanQR.java
@@ -9,6 +9,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.Intent;
+import android.widget.Toast;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 
 public class scanQR extends Fragment {
@@ -19,5 +23,28 @@ public class scanQR extends Fragment {
         View view =  inflater.inflate(R.layout.fragment_scan_qr, container, false);
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        new IntentIntegrator(getActivity()).initiateScan();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(getActivity(), "Cancelled", Toast.LENGTH_LONG).show();
+                // TODO
+            } else {
+                Toast.makeText(getActivity(), "Scanned: "+ result.getContents(), Toast.LENGTH_LONG).show();
+                // TODO
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
